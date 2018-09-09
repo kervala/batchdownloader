f32dc356575160ab4e606d00974c4e07e2b754a4
Kervala <983521+kervala@users.noreply.github.com>
2018-09-09 11:05:10 +0200

    Changed: Updated translations

--- translations/batchdownloader_fr.ts
+++ translations/batchdownloader_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fr_FR" sourcelanguage="en_GB">
+<TS version="2.1" language="fr_FR" sourcelanguage="en_GB">
 <context>
     <name>MainWindow</name>
     <message>
@@ -17,11 +17,11 @@
     </message>
     <message>
         <source>Destination folder</source>
-        <translation type="obsolete">Répertoire de destination</translation>
+        <translation>Répertoire de destination</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="obsolete">Télécharger</translation>
+        <translation>Télécharger</translation>
     </message>
     <message>
         <source>Messages</source>
@@ -29,55 +29,87 @@
     </message>
     <message>
         <source>Confirmation</source>
-        <translation>Confirmation</translation>
+        <translation type="vanished">Confirmation</translation>
     </message>
     <message>
         <source>The file already exists. Do you want to delete it?</source>
-        <translation>Le fichier existe déjà. Voulez-vous le supprimer ?</translation>
+        <translation type="vanished">Le fichier existe déjà. Voulez-vous le supprimer ?</translation>
     </message>
     <message>
         <source>%1 kB downloaded in %2 sec</source>
-        <translation>%1 ko téléchargés en %2 sec</translation>
+        <translation type="vanished">%1 ko téléchargés en %2 sec</translation>
     </message>
     <message>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>MainWindow</translation>
     </message>
     <message>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Source</translation>
     </message>
     <message>
         <source>URL</source>
-        <translation type="unfinished"></translation>
+        <translation>URL</translation>
     </message>
     <message>
         <source>First</source>
-        <translation type="unfinished"></translation>
+        <translation>Premier</translation>
     </message>
     <message>
         <source>Last</source>
-        <translation type="unfinished"></translation>
+        <translation>Dernier</translation>
     </message>
     <message>
         <source>Step</source>
-        <translation type="unfinished"></translation>
+        <translation>Incrément</translation>
     </message>
     <message>
         <source>Destination</source>
-        <translation type="unfinished"></translation>
+        <translation>Destination</translation>
     </message>
     <message>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">Fichier</translation>
     </message>
     <message>
         <source>Skip existing files</source>
-        <translation type="unfinished"></translation>
+        <translation>Passer les fichiers existant</translation>
     </message>
     <message>
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">Générer</translation>
+    </message>
+    <message>
+        <source>Filename parameter</source>
+        <translation>Paramètre de nom de fichier</translation>
+    </message>
+    <message>
+        <source>Detect from URL</source>
+        <translation>Détecter à partir de l&apos;URL</translation>
+    </message>
+    <message>
+        <source>Referrer</source>
+        <translation>Referrer</translation>
+    </message>
+    <message>
+        <source>User-agent</source>
+        <translation>User-agent</translation>
+    </message>
+    <message>
+        <source>Folder</source>
+        <translation>Répertoire</translation>
+    </message>
+    <message>
+        <source>Use last directory from URL as local directory</source>
+        <translation>Utiliser le dernier répertoire de l&apos;URL comme répertoire local</translation>
+    </message>
+    <message>
+        <source>Replace underscores by spaces</source>
+        <translation>Remplacer les underscores par des espaces</translation>
+    </message>
+    <message>
+        <source>Stop on error</source>
+        <translation>Arrêter après une erreur</translation>
     </message>
 </context>
 </TS>
