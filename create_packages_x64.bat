915bb701004ba6f7296c1df0c0f1268d66349a89
Kervala <983521+kervala@users.noreply.github.com>
2022-10-12 17:14:06 +0200

    Fixed: Compilation with Qt 6

--- create_packages_x64.bat
+++ create_packages_x64.bat
@@ -1,13 +1,12 @@
 @echo off
 
 :: To be sure Python and Qt are in PATH
-::set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\6.0.0\msvc2019_64\bin
-set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\5.15.2\msvc2019_64\bin
+set PATH=%PATH%;C:\Prog\Qt\6.3.1\msvc2019_64\bin
 
 set CURRENTDIR=%cd%
 
 :: Call VC++ command-line tools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 cd %CURRENTDIR%
 
@@ -16,7 +15,7 @@ rmdir /s /q package_x64
 mkdir package_x64
 
 cd package_x64
-cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_INSTALL_RUNTIMES=ON -G"NMake Makefiles"
 
 nmake package
 
